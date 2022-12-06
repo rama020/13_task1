27e0f2b2df425af7d51d488c0e4b7634690ef6e1
rama020 <118764128+rama020@users.noreply.github.com>
2022-12-06 16:49:35 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,20 @@
+"""
+date:6th dec 2022
+day:tuesday
+topic:task
+author:rama bhargavi
+"""
+#printing 1 to 10 numbers
+n=int(input())
+for i in range(1,n+1):
+    print(i,end=" ") #1 2 3 4 5 6 7 8 9 10
+ #2nd method
+# a=1
+while(a<=10):
+    print(a)
+    a+=1   
+#display evens m to n
+m=int(input())#2
+n=int(input())#10
+for i in range(m,n,2):
+    print(i,end=' ')#2 4 6 8
